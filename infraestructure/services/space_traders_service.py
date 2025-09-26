9afcebf71b6d02f0269298e6fdaac4e17b86472a
kuronekoshiroinu <ckenaco@gmail.com>
2025-09-26 19:47:04 -0400

    feat: space traders services

--- infraestructure/services/space_traders_service.py
+++ infraestructure/services/space_traders_service.py
@@ -1,3 +1,5 @@
+from zoneinfo import available_timezones
+
 import requests
 
 # from aplication.use_cases.ship_available_viewer import ShipAvailableViewer
@@ -5,7 +7,10 @@ from domain.constants import BASE_URL, AUTHORIZATION_HEADERS, ACCOUNT_HEADERS
 from domain.entities.account import Account
 from domain.entities.contract import Contract
 from domain.entities.engineered_asteroid import EngineeredAsteroid
+from domain.entities.ship_purchase import ShipPurchase
 from domain.entities.ship_purchase_ship import ShipPurchaseShip
+from domain.entities.ship_purchase_ship_nav import ShipPurchaseShipNav
+from domain.entities.ship_refuel import ShipRefuel
 from domain.entities.ships_available import ShipsAvailable
 from domain.entities.shipyard import Shipyard
 from domain.interfaces import TradersService
@@ -60,10 +65,60 @@ class SpaceTradersService(TradersService):
             )
         )
 
+    def purchase_ship(self, ship_type: str, waypoint_symbol:str) -> ShipPurchase:
+        url = f"{BASE_URL}/my/ships"
+        payload = {
+            "shipType": ship_type,
+            "waypointSymbol": waypoint_symbol
+        }
+        response = requests.post(
+            url,
+            headers=ACCOUNT_HEADERS,
+            json=payload
+        )
+        if response.status_code in (200, 201):
+            return ShipPurchase.from_dict(response.json()["data"])
+        else:
+            error_msg = response.json().get('error', {}).get('message', 'Error desconocido')
+            raise ValueError(f"Error al obtener ship purchase: {error_msg} statusCode: {response.status_code}")
+
+    def orbit_ship(self, ship_symbol:str) -> ShipPurchaseShipNav:
+        response = requests.post(f"{BASE_URL}/my/ships/{ship_symbol}/orbit",
+                                 headers=AUTHORIZATION_HEADERS)
+        if response.status_code == 200:
+            return ShipPurchaseShipNav.from_dict(response.json()["data"]["nav"])
+        else:
+            raise ValueError(f"Error al obtener datos: {response.status_code}")
+
+    def dock_ship(self, ship_symbol: str) -> ShipPurchaseShipNav:
+        response = requests.post(f"{BASE_URL}/my/ships/{ship_symbol}/dock",
+                                 headers=AUTHORIZATION_HEADERS)
+        if response.status_code == 200:
+            return ShipPurchaseShipNav.from_dict(response.json()["data"]["nav"])
+        else:
+            raise ValueError(f"Error al obtener datos: {response.status_code}")
+
+    def refuel_ship(self, ship_symbol: str) -> ShipRefuel:
+        return ShipRefuel.from_dict(
+            data=self._post_endpoint_response(
+                endpoint=f"/my/ships/{ship_symbol}/refuel",
+                headers=AUTHORIZATION_HEADERS,
+            )
+        )
+
     @classmethod
     def _get_endpoint_response(cls, endpoint: str, headers: dict):
         response = requests.get(f"{BASE_URL}{endpoint}",
                                 headers=headers)
+        if response.status_code == 200:
+            return response.json()["data"]
+        else:
+            raise ValueError(f"Error al obtener datos: {response.status_code} {response.json()}")
+
+    @classmethod
+    def _post_endpoint_response(cls, endpoint: str, headers: dict):
+        response = requests.post(f"{BASE_URL}{endpoint}",
+                                 headers=headers)
         if response.status_code == 200:
             return response.json()["data"]
         else:
@@ -76,11 +131,17 @@ if __name__ == "__main__":
 
     space = SpaceTradersService()
     ships = space.get_ships()
-    contract = space.get_contracts()
-    account = space.get_account()
-    # asteroids=space.find_engineered_asteroids(contract[0].terms.deliver[0].system_symbol)
-    shipyards_infos = space.find_shipyards(system_symbol=contract[0].terms.deliver[0].system_symbol)
-    available_ships_info = space.view_ship_available(system_symbol=contract[0].terms.deliver[0].system_symbol,
-                                                     waypoint_symbol=shipyards_infos[2].symbol)
-    pprint(contract)
-    # pprint(shipyards_infos)
+    #contracts = space.get_contracts()
+    #if not contracts:
+    #    raise ValueError("No hay contratos")
+    #account = space.get_account()
+    #asteroids=space.find_engineered_asteroids(contracts[0].terms.deliver[0].system_symbol)
+    #shipyards_infos = space.find_shipyards(system_symbol=contracts[0].terms.deliver[0].system_symbol)
+    #available_ships_info = space.view_ship_available(system_symbol=contracts[0].terms.deliver[0].system_symbol,
+                   #                        waypoint_symbol=shipyards_infos[2].symbol)
+    #orbiter=space.orbit_ship("GRAY-2")
+    #docker=space.dock_ship("AGENT_BLUE-6")
+    #pprint(orbiter)
+    #ship_purchaser=space.purchase_ship(ship_type="SHIP_MINING_DRONE",waypoint_symbol=available_ships_info.symbol)
+    refuel_ship=space.refuel_ship("AGENT_ROUGE-2")
+    pprint(refuel_ship)
